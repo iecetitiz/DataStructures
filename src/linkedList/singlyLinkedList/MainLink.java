97f24f62b278516fb054987cdae285825ee41468
iecetitiz <i.ecetitiz@gmail.com>
2022-10-05 22:22:29 +0300

    added two classes

---
+++ src/linkedList/singlyLinkedList/MainLink.java
@@ -0,0 +1,4 @@
+package linkedList.singlyLinkedList;
+
+public class MainLink {
+}
---
+++ src/linkedList/singlyLinkedList/SinglyLinkedList.java
@@ -0,0 +1,8 @@
+package linkedList.singlyLinkedList;
+
+public class SinglyLinkedList<T>{
+    private T data;
+    private SinglyLinkedList<T> next;
+}
+
+
