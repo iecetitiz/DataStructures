ab0ebcbd437bbda539dab610578a3dfa64b665fb
iecetitiz <i.ecetitiz@gmail.com>
2022-11-06 22:45:05 +0300

    algorithm analysis examples are added

--- src/random/Main.java
+++ src/random/Main.java
@@ -7,11 +7,19 @@ public class Main {
         //c1.getColor();
         //System.out.println("ece");
 
-        int a = 5;
-        if(a % 2 != 0) {
-            System.out.println((a /2) +1 );
+        //int a = 5;
+        //if(a % 2 != 0) {
+        //  System.out.println((a /2) +1 );
+        //}
+        int i = 1;
+        int n = 50;
+        while (i < n) {
+            int j = n;
+            while (j > 0) {
+                j = j / 2;
+                System.out.println(j);
+            }
+            i = i * 2;
         }
-
-
     }
 }
