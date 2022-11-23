f878a89d8582a5db601845bf05fb39416fd0e836
iecetitiz <i.ecetitiz@gmail.com>
2022-11-23 13:29:06 +0300

    implicit type conversation is added

---
+++ src/lab_SE309/midtermprep/Precedence.java
@@ -0,0 +1,18 @@
+package lab_SE309.midtermprep;
+
+public class Precedence {
+    public static void main(String[] args) {
+        int b = 3;
+        int c = 4;
+        int a = 5+-b*c;
+        System.out.println(a);
+
+        int A = Integer.MAX_VALUE;
+        int C = Integer.MAX_VALUE;
+        int B = Integer.MIN_VALUE;
+        int D = Integer.MIN_VALUE;
+
+        System.out.println(A + B + C + D);
+        System.out.println(A + C);
+    }
+}
