01735b74a866a913b977061a9487e91298efa335
iecetitiz <i.ecetitiz@gmail.com>
2022-12-10 08:11:06 +0300

    heap class skeleton is created

--- src/heap/BinaryHeap.java
+++ src/heap/BinaryHeap.java
@@ -7,4 +7,43 @@ public class BinaryHeap <AnyType extends Comparable<? super AnyType>>{
  { /* See online code */ }
     public BinaryHeap( AnyType [ ] items )
 { /* Figure 6.14 */ }
+    public void insert(AnyType x) {
+
+    }
+
+    public AnyType findMin() {
+
+    }
+
+    public AnyType deleteMin() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public void makeEmpty() {
+
+    }
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private int currentSize; //number of elements in heap
+    private AnyType[] array; //heap array
+
+    private void percolateDown(int hole) {
+
+    }
+
+    private void buildHeap() {
+
+    }
+
+    private void enlargeArray(int newSize) {
+        
+    }
+
+
+
 }
