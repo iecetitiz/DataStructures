06d166968854aa63d85b3f859020383750254476
iecetitiz <i.ecetitiz@gmail.com>
2023-01-04 23:49:09 +0300

    some dummy codes are added

---
+++ src/Edible.java
@@ -0,0 +1,34 @@
+public class Edible {
+
+    public class() {
+        int sum;
+        int divide;
+        double require;
+
+    }
+
+    public circle() {
+        double diameter;
+
+    }
+
+    public double getDiameter;
+    public double getPerimeter;
+    public void howTofind() {
+
+    }
+
+    public static void main() {
+
+    }
+
+    public abstract void getName() {
+
+    }
+
+    public static void getSource() {
+
+
+
+    }
+}
